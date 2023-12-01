cc5da4b98797d020662aa37a55ae30b11e961e9c
kylewu12 <kyle.wu12@gmail.com>
2023-11-30 19:19:24 -0800

    Work so far EDA

--- Scripts/kyle_eda.R
+++ Scripts/kyle_eda.R
@@ -2,6 +2,15 @@ library(tidyverse)
 
 network_data <- read_csv("Data/network_data_file_columns_named.csv")
 
+attack_distribution_binomial <- network_data |>
+  select(attack) |> 
+  mutate(attack = as_factor(attack),
+         attack = if_else(attack == "normal", "normal", "attack")) |>
+  summarise(count = n(), .by = attack) |>
+  ggplot() +
+  geom_bar(aes(x = attack, y = count, fill = attack), stat = "identity")
+
+
 duration_distribution <- summary(network_data$duration)
 
 protocoltype_distribution <- network_data |>
@@ -10,3 +19,18 @@ protocoltype_distribution <- network_data |>
   summarise(count = n(), .by = protocoltype) |>
   ggplot() +
   geom_bar(aes(x = protocoltype, y = count, fill = protocoltype), stat = "identity")
+
+
+
+donor_data <- read_csv("Data/KDD2014_donors_10feat_nomissing_normalised.csv")
+
+class_distribution <- donor_data |>
+  mutate(class = as_factor(class)) |>
+  summarise(count = n(), .by = class) |>
+  ggplot() + 
+  geom_bar(aes(x = class, y = count, fill = class), stat = "identity")
+
+network_intrusion_data <- read_csv("Data/UNSW_NB15_training-set.csv") |>
+  mutate(attack_cat = if_else(attack_cat == "Normal", "Normal", "Attack"))
+
+nsw_attack_distribution
