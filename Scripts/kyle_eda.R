d99dcfb359581445a835cef1475252415aedf1fc
kylewu12 <kyle.wu12@gmail.com>
2023-11-30 17:36:29 -0800

    Very initial EDA, data may not be ideal for unsupervised anomaly detection

--- Scripts/kyle_eda.R
+++ Scripts/kyle_eda.R
@@ -1,2 +1,12 @@
 library(tidyverse)
 
+network_data <- read_csv("Data/network_data_file_columns_named.csv")
+
+duration_distribution <- summary(network_data$duration)
+
+protocoltype_distribution <- network_data |>
+  select(protocoltype) |>
+  mutate(protocoltype = as_factor(protocoltype)) |>
+  summarise(count = n(), .by = protocoltype) |>
+  ggplot() +
+  geom_bar(aes(x = protocoltype, y = count, fill = protocoltype), stat = "identity")
