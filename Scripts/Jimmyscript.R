84b4bfd129f82b6c0cbb455a6665d5f6b70b3147
jimmydysart <117496285+jimmydysart@users.noreply.github.com>
2023-12-09 12:01:28 -0800

    Update Jimmyscript.R

--- Scripts/Jimmyscript.R
+++ Scripts/Jimmyscript.R
@@ -5,3 +5,9 @@ library(corrplot)
 library(readr)
 bc_data_prepared <- read_csv("Data/bc_data_prepared.csv")
 View(bc_data_prepared)
+
+table(bc_data_prepared$diagnosis)
+
+library(dplyr)
+bc_data_prepared <- bc_data_prepared %>%
+  mutate(diagnosis = recode(diagnosis, B = 'Benign', M = 'Malignant' ))
